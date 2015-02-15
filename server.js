6b5b93902608c1e71d971a659e26e7de445f858d
Stephen Done <srdone@gmail.com>
2015-02-14 18:14:52 -0700

    json responses

--- server.js
+++ server.js
@@ -10,27 +10,49 @@ var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-//json data - getters only - not yet in json
+//json data - getters only
 app.get('/name', function(req, res, next) {
   //note that send ends the request / response cycle and stops calling middleware - it DOES send JSON
   res.send({name: 'Stephen Done'});
 });
+
 app.get('/location', function(req, res) {
-  res.send('Somewhere in your head');
+  //Or we can use res.json() - it forces JSON response
+  res.json({location: 'Somewhere in your head'});
 });
+
 app.get('/hobbies', function(req, res) {
-  res.write('reading\n');
-  res.write('singing\n');
-  res.end();
+  var hobbies = ['reading', 'singing', 'hacking', 'learning cool stuff'];
+
+  res.json({hobbies: hobbies});
 });
-//notice how the format of the data changes between doing a write and just sending
-//res.end is required - otherwise it never completes a response
+
 app.get('/occupations', function(req, res) {
-  res.write('Business Analyst - University of Phoenix');
-  res.end();
+  var wellsFargo = {
+    company: 'Wells Fargo',
+    position: 'Operations Analyst',
+    start: Date('September 1, 2007'),
+    end: Date('September 30, 2008')
+  };
+  var universityOfPhoenix = {
+    company: 'University of Phoenix',
+    position: 'Business Analyst',
+    start: Date('October 1, 2008'),
+    end: Date('October 31, 2009')
+  };
+  var universityOfPhoenix2 = {
+    company: 'University of Phoenix',
+    position: 'Business Analyst II - Team Lead',
+    start: Date('November 1, 2009')
+  };
+
+  var allPositions = [wellsFargo, universityOfPhoenix, universityOfPhoenix2];
+
+  res.json(allPositions);
 });
+
 app.get('/occupations/latest', function(req, res) {
-  res.send('Full Stack Software Developer');
+  res.send({position: 'Full Stack Software Developer'});
 });
 
 //add static routes
