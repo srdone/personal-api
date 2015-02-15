c9422301c3e7a776c45529d6a4298bbabda20a41
Stephen Done <srdone@gmail.com>
2015-02-14 17:03:23 -0700

    very basic endpoints

--- server.js
+++ server.js
@@ -10,6 +10,28 @@ var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+//json data - getters only - not yet in json
+app.get('/name', function(req, res) {
+  res.send('Stephen Done');
+});
+app.get('/location', function(req, res) {
+  res.send('Somewhere in your head');
+});
+app.get('/hobbies', function(req, res) {
+  res.write('reading\n');
+  res.write('singing\n');
+  res.end();
+});
+//notice how the format of the data changes between doing a write and just sending
+//res.end is required - otherwise it never completes a response
+app.get('/occupations', function(req, res) {
+  res.write('Business Analyst - University of Phoenix');
+  res.end();
+});
+app.get('/occupations/latest', function(req, res) {
+  res.send('Full Stack Software Developer');
+});
+
 //add static routes
 app.use(express.static('./public'));
 
